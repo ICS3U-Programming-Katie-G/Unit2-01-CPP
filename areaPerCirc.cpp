154ecfa3cb6a11250e029410f625bac8ce937808
Kaitlin Green <kaitlin.green@stu.ocsb.ca>
2022-09-25 03:20:36 +0000

    completed this code in CPP to calculate the area and circumference of a circle while listening to Right Hand Man from the Hamilton soundtrack. 10/10 would recommend.

---
+++ areaPerCirc.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2022 Katie G All rights reserved.
+//.
+// Created by: Katie
+// Date: September 16th, 2022
+// this program calculates the area and circumference
+// of a circle with a set radius of 4.
+
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "A circle has the radius of 4:\n";
+    std::cout << "The area is: " << M_PI * pow(4, 2) << " mm^2\n";
+    std::cout << "The circumference is: " << 2 * M_PI * 4 << " mm\n";
+}
